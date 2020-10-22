8bdb9b1dbabb0b44305b438c77f159684ec29b71
jeffery71 <jefferynthu@gmail.com>
2020-10-22 23:26:58 +0800

    fix

--- moi/models.py
+++ moi/models.py
@@ -71,15 +71,15 @@ class NetworkSliceSubnet(models.Model):
     nsInfo = models.ForeignKey(NsInfo, null=True, on_delete=models.CASCADE, related_name='nsInfo')
     sliceProfileList = models.ManyToManyField(SliceProfileList, related_name='sliceProfileList')
     #modify
-    mongodb = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=True)
-    nrfd = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=True)
-    amfd = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=True)
-    smfd = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=True)
-    udrd = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=True)
-    pcfd = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=True)
-    udmd = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=True)
-    nssfd = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=True)
-    aufd = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=True)
+    mongodb = models.UUIDField(default=uuid.uuid4)
+    nrfd = models.UUIDField(default=uuid.uuid4, editable=True)
+    amfd = models.UUIDField(default=uuid.uuid4, editable=True)
+    smfd = models.UUIDField(default=uuid.uuid4, editable=True)
+    udrd = models.UUIDField(default=uuid.uuid4, editable=True)
+    pcfd = models.UUIDField(default=uuid.uuid4, editable=True)
+    udmd = models.UUIDField(default=uuid.uuid4, editable=True)
+    nssfd = models.UUIDField(default=uuid.uuid4, editable=True)
+    aufd = models.UUIDField(default=uuid.uuid4, editable=True)
     #modify
 
 class AMFSet(models.Model):
